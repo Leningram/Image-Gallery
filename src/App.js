34241079da3362c3ad440f17263190dddcc6e7b9
Kanat Yes <yes.kanat@gmail.com>
2021-06-22 15:01:09 +0300

    The initial images are taken from server now

--- src/App.js
+++ src/App.js
@@ -2,103 +2,106 @@ import React, { Component } from "react";
 import ImageGallery from "./components/image-gallery/image-gallery";
 import AddForm from "./components/image-add/image-add";
 
-// const addImage = (imgSrc) => {
-//     const newItem = {
-//         url: imgSrc,
-//         id: data.length + 1
-//     };
-//     data = data.concat(newItem);
-// };
-
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [
-                {
-                    url: "https://images.unsplash.com/photo-1593901138884-02ee723a96f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2002&q=80",
-                    width: 640,
-                    height: 426,
-                    id: 1
-                },
-                {
-                    url: "https://images.unsplash.com/photo-1562170824-b547dae88b97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
-                    width: 1920,
-                    height: 1200,
-                    id: 2
-                },
-                {
-                    url: "https://images.unsplash.com/photo-1554791072-277bba66f220?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1927&q=80",
-                    width: 640,
-                    height: 426,
-                    id: 3
-                },
-                {
-                    url: "https://images.unsplash.com/photo-1510739859545-e7b9e979de86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-                    width: 436,
-                    height: 650,
-                    id: 4
-                },
-                {
-                    url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/493550740.jpg",
-                    width: 600,
-                    height: 400,
-                    id: 5
-                },
-                {
-                    url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/448964008.jpg",
-                    width: 509,
-                    height: 339,
-                    id: 6
-                },
-                {
-                    url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/448964011.jpg",
-                    width: 900,
-                    height: 450,
-                    id: 7
-                },
-                {
-                    url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/493550755.jpg",
-                    width: 480,
-                    height: 640,
-                    id: 8
-                },
-                {
-                    url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/448964013.jpg",
-                    width: 472,
-                    height: 640,
-                    id: 9
-                },
-                {
-                    url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/493550745.jpg",
-                    width: 640,
-                    height: 425,
-                    id: 10
-                }
-            ]
+            data: [],
+            isFetching: true,
+            error: null
+            // data: [
+            //     {
+            //         url: "https://images.unsplash.com/photo-1593901138884-02ee723a96f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2002&q=80",
+            //         width: 640,
+            //         height: 426,
+            //         id: 1
+            //     },
+            //     {
+            //         url: "https://images.unsplash.com/photo-1562170824-b547dae88b97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
+            //         width: 1920,
+            //         height: 1200,
+            //         id: 2
+            //     },
+            //     {
+            //         url: "https://images.unsplash.com/photo-1554791072-277bba66f220?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1927&q=80",
+            //         width: 640,
+            //         height: 426,
+            //         id: 3
+            //     },
+            //     {
+            //         url: "https://images.unsplash.com/photo-1510739859545-e7b9e979de86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
+            //         width: 436,
+            //         height: 650,
+            //         id: 4
+            //     },
+            //     {
+            //         url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/493550740.jpg",
+            //         width: 600,
+            //         height: 400,
+            //         id: 5
+            //     },
+            //     {
+            //         url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/448964008.jpg",
+            //         width: 509,
+            //         height: 339,
+            //         id: 6
+            //     },
+            //     {
+            //         url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/448964011.jpg",
+            //         width: 900,
+            //         height: 450,
+            //         id: 7
+            //     },
+            //     {
+            //         url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/493550755.jpg",
+            //         width: 480,
+            //         height: 640,
+            //         id: 8
+            //     },
+            //     {
+            //         url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/448964013.jpg",
+            //         width: 472,
+            //         height: 640,
+            //         id: 9
+            //     },
+            //     {
+            //         url: "https://don16obqbay2c.cloudfront.net/frontend-test-task/images/493550745.jpg",
+            //         width: 640,
+            //         height: 425,
+            //         id: 10
+            //     }
+            // ]
         };
         this.addImage = this.addImage.bind(this);
 
         this.maxId = 11; //Счетчик для добавления уникальных id к новым элементам
     }
 
+    // Получаем начальные изображения по ссылки из тестового задания
+    componentDidMount() {
+        fetch("https://don16obqbay2c.cloudfront.net/frontend-test-task/gallery-images.json")
+            .then((response) => response.json())
+            .then((result) => this.setState({ data: result.galleryImages }));
+    }
+    //
     addImage(url) {
-        if (url) {
-            const newImage = {
-                url,
-                width: 640,
-                height: 425,
-                id: this.maxId++
-            };
-            this.setState(({ data }) => {
-                const newArray = [...data, newImage];
-                return {
-                    data: newArray
-                };
-            });
-        } else {
+        if (!url) {
             alert("Введите URL");
+            return;
         }
+
+        const newImage = {
+            url,
+            width: 640,
+            height: 425,
+            id: this.maxId++
+        };
+        this.setState(({ data }) => {
+            const newArray = [...data, newImage];
+            return {
+                data: newArray
+            };
+        });
     }
 
     render() {
