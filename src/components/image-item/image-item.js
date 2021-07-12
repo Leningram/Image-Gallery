0191a1cf5add2135c47d0adec8015fd0d3bb71b2
Kanat Yes <yes.kanat@gmail.com>
2021-07-12 14:14:50 +0300

    loader changed

--- src/components/image-item/image-item.js
+++ src/components/image-item/image-item.js
@@ -16,9 +16,12 @@ export default class ImageItem extends Component {
 
     render() {
         const { index, img, onDelete, showImage } = this.props; //деструктурируем пропсы для сокращенной записи
+        console.log(this.state.loading);
         const content = this.state.loading ? (
-            <div className="loader-container">
-                <div className="loader-wheel"></div>
+            <div className="loadingio-spinner-rolling-sfoaij7v4zk">
+                <div className="ldio-nqybr7h0e6">
+                    <div></div>
+                </div>
             </div>
         ) : (
             <div>
@@ -32,6 +35,7 @@ export default class ImageItem extends Component {
                 <DeleteIcon onClick={() => onDelete(index)} />
             </div>
         );
+
         return <div className="pics">{content}</div>;
     }
 }
